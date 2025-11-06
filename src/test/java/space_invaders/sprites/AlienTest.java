db53d4ddc486f2d331f057ba90c6fdbb9576c158
jaimeprieto <jaime.prieto@alumnos.upm.es>
2025-11-06 18:45:28 +0100

    initial commit

---
+++ src/test/java/space_invaders/sprites/AlienTest.java
@@ -0,0 +1,14 @@
+package space_invaders.sprites;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AlienTest {
+
+    @org.junit.jupiter.api.Test
+    void act() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBomb() {
+    }
+}
